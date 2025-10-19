72cd02fba387c6b0b207a0ccbaefee9f025096e9
JoshuaHassell05 <157650123+JoshuaHassell05@users.noreply.github.com>
2025-10-19 22:39:11 +0000

    define score variables and vlaues

--- .github/workflows/task1.c
+++ .github/workflows/task1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(){
     int score;
+    int td2 = 8;
+    int td1 = 7;
+    int td = 6;
+    int fg = 3;
+    int safety = 2;
     printf("Enter the score to see all possible combinations of NFL scoring plays: ");
     scanf("%d", &score);
     return 0;
