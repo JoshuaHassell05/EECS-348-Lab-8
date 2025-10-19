b41451c588666b0b0230a251aa24c6d69ed7e22f
JoshuaHassell05 <157650123+JoshuaHassell05@users.noreply.github.com>
2025-10-19 23:30:52 +0000

    changes to specifiers and error checking for scale

--- .github/workflows/task2.c
+++ .github/workflows/task2.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 int main(){
-    int temp;
+    double temp;
+    double tempC;
     char scale;
     char convert;
     printf("Enter the temperature vlaue: ");
-    scanf("%d", &temp);
+    scanf("%lf", &temp);
     printf("Enter the original scale (C, F, or K): ");
-    scanf("%c", &scale);
+    scanf(" %c", &scale);
     printf("Enter the scale to convert to (C, F, or K): ");
-    scanf("%c", &convert);
-    if (scale == "C" || scale == "F" || scale == "K" || convert == "C" || convert == "F" || convert == "K"){
-        printf("Invalid Scale");
+    scanf(" %c", &convert);
+    if (scale == 'C' || scale == 'c')
+        tempC = temp;
+    else if (scale == 'F' || scale == 'f')
+        tempC = (temp - 32) * 5.0 / 9.0;
+    else if (scale == 'K' || scale == 'k')
+        tempC = temp - 273.15;
+    else {
+        printf("Invalid Scale\n");
+        return 0;
     }
     return 0;
 }
