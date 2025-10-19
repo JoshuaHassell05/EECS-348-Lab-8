d4718371a7cd7b465c0f652c783ec6a9b2da975c
JoshuaHassell05 <157650123+JoshuaHassell05@users.noreply.github.com>
2025-10-19 23:37:52 +0000

    Added conversion from C to target

--- .github/workflows/task2.c
+++ .github/workflows/task2.c
@@ -5,20 +5,36 @@ int main(){
     char scale;
     char convert;
     double converted;
-    printf("Enter the temperature vlaue: ");
+    printf("Enter the temperature value: ");
     scanf("%lf", &temp);
     printf("Enter the original scale (C, F, or K): ");
     scanf(" %c", &scale);
     printf("Enter the scale to convert to (C, F, or K): ");
     scanf(" %c", &convert);
-    if (scale == 'C' || scale == 'c')
+    if (scale == 'C' || scale == 'c'){
         tempC = temp;
-    else if (scale == 'F' || scale == 'f')
+    }
+    else if (scale == 'F' || scale == 'f'){
         tempC = (temp - 32) * 5.0 / 9.0;
-    else if (scale == 'K' || scale == 'k')
+    }
+    else if (scale == 'K' || scale == 'k'){
         tempC = temp - 273.15;
+    }
     else {
         printf("Invalid Scale\n");
+        return 0;
+    }
+    if (convert == 'C' || convert == 'c'){
+        converted = tempC;
+    }
+    else if (convert == 'F' || convert == 'f'){
+        converted = tempC * 9.0 / 5.0 + 32;
+    }
+    else if (convert == 'K' || convert == 'k'){
+        converted = tempC + 273.15;
+    }
+    else {
+        printf("Invalid scale\n");
         return 0;
     }
     printf("Converted temperature: %.2f %c\n", converted, convert);
