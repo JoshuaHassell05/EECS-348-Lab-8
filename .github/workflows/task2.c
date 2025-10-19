fa24d5955623d58420d003a19243e449191d6830
JoshuaHassell05 <157650123+JoshuaHassell05@users.noreply.github.com>
2025-10-19 23:13:14 +0000

    print to user and take in input

---
+++ .github/workflows/task2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int temp;
+    char scale;
+    char convert;
+    printf("Enter the temperature vlaue: ");
+    scanf("%d", &temp);
+    printf("Enter the original scale (C, F, or K): ");
+    scanf("%c", &scale);
+    printf("Enter the scale to convert to (C, F, or K): ");
+    scanf("%c", &convert);
+    return 0;
+}
